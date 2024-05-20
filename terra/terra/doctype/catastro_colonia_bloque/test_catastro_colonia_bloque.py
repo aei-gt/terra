40a5371a11931493fb42f661461b6361057ffe12
Alquimia E Integra <info@aei.gt>
2024-05-19 19:26:45 -0600

    Creación modulos Terra Iniciales

---
+++ terra/terra/doctype/catastro_colonia_bloque/test_catastro_colonia_bloque.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, AEI and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testcatastro_colonia_bloque(FrappeTestCase):
+	pass
---
+++ terra/terra/doctype/catastro_contribuyente_detalle/catastro_contribuyente_detalle.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, AEI and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("catastro_contribuyente_detalle", {
+// 	refresh(frm) {
+
+// 	},
+// });
