c02307116074ccf9a825e3976a2c4485ecc367c4
raza9100 <ahmedrazapk>
2024-09-12 18:39:34 +0500

    fixes

--- terra/terra/doctype/catastro_contribuyente_detalles/catastro_contribuyente_detalles.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, AEI and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class catastro_contribuyente_detalles(Document):
-	pass
