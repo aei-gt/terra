0ab1a16b8eeb94d9303e25af1e1679da49d2209a
rajaabdullaharshad <rajaabdullaharshad@gmail.com>
2024-09-19 06:14:29 +0000

    changes

--- terra/terra/doctype/catastro_reporte/catastro_reporte.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, AEI and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("catastro_reporte", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- terra/terra/doctype/catastro_reporte/test_catastro_reporte.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, AEI and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class Testcatastro_reporte(FrappeTestCase):
-	pass
