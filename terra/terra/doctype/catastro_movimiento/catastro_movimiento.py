ba1bf2cc1a74af2eb3c3bfab8f16c450da760c1a
rajaabdullaharshad <rajaabdullaharshad@gmail.com>
2024-09-20 19:47:23 +0500

    new changes and add feature

--- terra/terra/doctype/catastro_movimiento/catastro_movimiento.py
+++ terra/terra/doctype/catastro_movimiento/catastro_movimiento.py
@@ -2,43 +2,36 @@ import frappe
 from frappe.model.document import Document
 
 class catastro_movimiento(Document):
-     def validate(self):
+    def validate(self):
         if self.catastro_movimiento_tipo == "1-INSCRIPCION NUEVA":
-                inmueble_doc= frappe.get_doc("catastro_inmueble",self.id_catastro)
-       
-                doc = frappe.get_doc({
-                    'doctype': 'catastro_inmueble',
-                    'propietario': inmueble_doc.propietario,
-                    'finca': inmueble_doc.finca,
-                    'folio': inmueble_doc.folio,
-                    'libro': inmueble_doc.libro,
-                    'ubicacion_catastral':inmueble_doc.ubicacion_catastral,
-                    
-                })
-                doc.insert()
-                return frappe.msgprint(f"New catastro_inmueble = {doc.name}and the  Tarjeta = {doc.tarjeta}")
-
+            doc = frappe.get_doc({
+                'doctype': 'catastro_inmueble',
+                'propietario': self.propietario,
+                'finca': self.finca,
+                'folio': self.folio,
+                'libro': self.libro,
+                'registro': self.registro,
+                'ubicacion_catastral': self.ubicación_catastral_dirección,
+            })
+            doc.insert()
+            return frappe.msgprint(f"New catastro_inmueble created: {doc.name}. Tarjeta = {doc.tarjeta}")
 
         elif self.catastro_movimiento_tipo == "4-ACTUALIZACION DE VALOR":
-            fields = ["valor_del_terreno", "valor_de_construccion", "valor_de_cultivo", "valor_total"]
-            catastro_values = frappe.get_value("catastro_inmueble", {"name": self.id_catastro}, fields)
+            if self.id_catastro:
+                fields = ["valor_del_terreno", "valor_de_construccion", "valor_de_cultivo", "valor_total"]
+                catastro_values = frappe.get_value("catastro_inmueble", self.id_catastro, fields)
+                if catastro_values:
+                    updates = {}
+                    if catastro_values[0] < self.valor_del_terreno:
+                        updates["valor_del_terreno"] = self.valor_del_terreno
+                    if catastro_values[1] < self.valor_de_construccion:
+                        updates["valor_de_construccion"] = self.valor_de_construccion
+                    if catastro_values[2] < self.valor_de_cultivo:
+                        updates["valor_de_cultivo"] = self.valor_de_cultivo
+                    if catastro_values[3] < self.valor_total:
+                        updates["valor_total"] = self.valor_total
+                    
+                    if updates:
+                        frappe.db.set_value("catastro_inmueble", self.id_catastro, updates)
+                        frappe.msgprint(title="Updated fields Successfully", msg="These Are All The Updated fields: {}".format(', '.join(updates.keys())), indicator='green')
 
-            if catastro_values:
-                updates = {}
-                
-                # Compare and update values
-                if catastro_values[0] < self.valor_del_terreno:
-                    updates["valor_del_terreno"] = self.valor_del_terreno
-                if catastro_values[1] < self.valor_de_construccion:
-                    updates["valor_de_construccion"] = self.valor_de_construccion
-                if catastro_values[2] < self.valor_de_cultivo:
-                    updates["valor_de_cultivo"] = self.valor_de_cultivo
-                if catastro_values[3] < self.valor_total:
-                    updates["valor_total"] = self.valor_total
-
-                # Apply updates if there are any changes
-                if updates:
-                    frappe.db.set_value("catastro_inmueble", self.id_catastro, updates)
-                    frappe.msgprint(f"Updated fields: {', '.join(updates.keys())}")
-            else:
-                frappe.msgprint(f"Document not found for ID {self.id_catastro}")
