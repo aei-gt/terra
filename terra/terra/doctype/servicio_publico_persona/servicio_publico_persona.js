c23d33125ce2e811575afc7732a68c520b6b09d7
Alquimia E Integra <111803510+aei-gt@users.noreply.github.com>
2024-12-27 11:02:24 -0600

    Delete terra/terra/doctype/servicio_publico_persona directory
    not integrated

--- terra/terra/doctype/servicio_publico_persona/servicio_publico_persona.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, AEI and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("servicio_publico_persona", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- terra/terra/doctype/servicio_publico_persona/test_servicio_publico_persona.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, AEI and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class Testservicio_publico_persona_persona(FrappeTestCase):
-	pass
