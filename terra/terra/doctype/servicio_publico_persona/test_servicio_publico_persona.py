0f92c46d4627bcbcbb47e86d67a0ce685d4b3f7f
aei-gt <info@aei.gt>
2024-12-27 03:16:14 +0000

    Changes to licences

---
+++ terra/terra/doctype/servicio_publico_persona/test_servicio_publico_persona.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, AEI and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testservicio_publico_persona_persona(FrappeTestCase):
+	pass
