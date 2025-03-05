0930f06a2ee894a910edb1553dfe55bcbedc6341
aei-gt <info@aei.gt>
2025-03-05 17:29:04 +0000

    Actualizacion tablas

--- terra/terra/doctype/usergl/usergl.js
+++ terra/terra/doctype/usergl/usergl.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, AEI and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("User GL", {
+// frappe.ui.form.on("UserGL", {
 // 	refresh(frm) {
 
 // 	},
